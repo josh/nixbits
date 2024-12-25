d4494120fed60378a1efbcac349a9f8e52daea06
Joshua Peek <josh@users.noreply.github.com>
2024-12-25 14:10:49 -0800

    Don't really need to test trivial alias

--- pkgs/deadsymlinks.nix
+++ pkgs/deadsymlinks.nix
@@ -1,39 +1,16 @@
 {
   lib,
   writeShellApplication,
-  runCommand,
   findutils,
-  testers,
 }:
-let
-  deadsymlinks = writeShellApplication {
-    name = "deadsymlinks";
-    runtimeEnv = {
-      PATH = lib.strings.makeBinPath [
-        findutils
-      ];
-    };
-    text = ''
-      find . -xtype l
-    '';
-
-    passthru.tests = {
-      usage = testers.testEqualContents {
-        assertion = "deadsymlinks works";
-        expected = runCommand "expected" { } ''
-          echo "./bar-symlink" >$out
-        '';
-        actual = runCommand "actual" { nativeBuildInputs = [ deadsymlinks ]; } ''
-          echo 42 >foo
-          echo 42 >bar
-          ln -s foo foo-symlink
-          ln -s bar bar-symlink
-          rm bar
-
-          deadsymlinks >$out
-        '';
-      };
-    };
+writeShellApplication {
+  name = "deadsymlinks";
+  runtimeEnv = {
+    PATH = lib.strings.makeBinPath [
+      findutils
+    ];
   };
-in
-deadsymlinks
+  text = ''
+    find . -xtype l
+  '';
+}
