033d32154eb72955a4078069b254c0a5b898c18a
Joshua Peek <josh@users.noreply.github.com>
2025-01-12 15:42:04 -0800

    Use mkDerivation and add tests

--- pkgs/age-decrypt.nix
+++ pkgs/age-decrypt.nix
@@ -1,25 +1,68 @@
 {
-  runCommand,
+  stdenv,
   makeWrapper,
+  runCommand,
   nixbits,
   age,
   age-input ? nixbits.empty-file,
 }:
-runCommand "age-decrypt"
-  {
-    nativeBuildInputs = [ makeWrapper ];
-    meta = age.meta // {
-      mainProgram = "age-decrypt";
-    };
-  }
-  ''
-    if [ ! -f ${age-input} ]; then
-      echo "error: ${age-input} not a file" >&2
+stdenv.mkDerivation (finalAttrs: {
+  name = "age-decrypt";
+  inherit (age) version;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  AGE_INPUT = age-input;
+
+  buildCommand = ''
+    if [ ! -f "$AGE_INPUT" ]; then
+      echo "error: $AGE_INPUT not a file" >&2
       exit 1
     fi
 
     mkdir -p $out/bin
     makeWrapper ${age}/bin/age $out/bin/age-decrypt \
       --add-flags "--decrypt" \
-      --append-flags "${age-input}"
-  ''
+      --append-flags "$AGE_INPUT"
+  '';
+
+  meta = {
+    inherit (age.meta)
+      homepage
+      description
+      license
+      platforms
+      ;
+    mainProgram = "age-decrypt";
+  };
+
+  passthru.tests =
+    let
+      identity = builtins.toFile "key.txt" ''
+        AGE-SECRET-KEY-1XT05JRC9UUPTZET3YHH5EYC8Z4QKMQN3NXP4E85KG47946U529QQFL43S2
+      '';
+      data = builtins.toFile "data.age" ''
+        -----BEGIN AGE ENCRYPTED FILE-----
+        YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBEL1hLRU1ZbE1hcEYybXp0
+        SkFxOUVqVkpaOTA0YWthOGxTY0t2aDA0dldZCnJkTGdPTHR1U0FpaXJDTkxZc1dG
+        d2xQWXMxYXZRQm9XUnZ5UndaY1FsYXMKLS0tIHRsNVVTUDhMemhEWFJZUEFXNzV5
+        SkMwbmt0MU96a0s2UUV4N2UvSWNkazgKvEqwr1zxWWFHz7NiHfgLQXsJcg7O8SJN
+        AQxIIbneSxQvVy5k
+        -----END AGE ENCRYPTED FILE-----
+      '';
+      age-decrypt = finalAttrs.finalPackage.overrideAttrs { AGE_INPUT = "${data}"; };
+    in
+    {
+      decrypt = runCommand "test-age-decrypt" { nativeBuildInputs = [ age-decrypt ]; } ''
+        age-decrypt --identity ${identity} >data.txt
+        if [ "$(cat data.txt)" = "foo" ]; then
+          cp data.txt $out
+        else
+          echo "expected: foo"
+          echo -n "actual:"
+          cat data.txt
+          exit 1
+        fi
+      '';
+    };
+})
