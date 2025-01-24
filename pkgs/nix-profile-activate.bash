dd9adb3fa0bbac928201cecd4e283f7e67fcca4d
Joshua Peek <josh@users.noreply.github.com>
2025-01-24 13:00:58 -0700

    Extract nix-profile-run-hooks script

--- pkgs/nix-profile-activate.bash
+++ pkgs/nix-profile-activate.bash
@@ -12,45 +12,6 @@ if [ ! -f "$new_profile/manifest.json" ]; then
   exit 1
 fi
 
-run_hooks() {
-  echo "Running $1" >&2
-  local code=0
-  if [ -x "$new_profile/share/nix/hooks/${1}" ]; then
-    echo "+ $1" >&2
-    if ! "$new_profile/share/nix/hooks/${1}"; then
-      code=1
-    fi
-  fi
-  if [ -d "$new_profile/share/nix/hooks/${1}.d" ]; then
-    for script in "$new_profile/share/nix/hooks/${1}.d"/*; do
-      echo "+ $(basename "$script")" >&2
-      if ! "$script"; then
-        code=1
-      fi
-    done
-  fi
-  if [ $code -ne 0 ]; then
-    echo "$1 hooks failed" >&2
-    exit $code
-  fi
-}
-
-run_old_hooks() {
-  local code=0
-  if [ -d "$new_profile/libexec/$1" ]; then
-    for script in "$new_profile/libexec/$1"/*; do
-      echo "+ $(basename "$script" ".sh")" >&2
-      if ! "$script"; then
-        code=1
-      fi
-    done
-  fi
-  if [ $code -ne 0 ]; then
-    echo "$1 old hooks failed" >&2
-    exit $code
-  fi
-}
-
 profile_n() {
   local m=0
   for file in "$nix_profile_dir"/profile-*-link; do
@@ -69,16 +30,11 @@ if [ "$old_profile" == "$new_profile" ]; then
   exit 0
 fi
 
-export NIX_NEW_PROFILE="$new_profile"
-export NIX_OLD_PROFILE="$old_profile"
-
-run_hooks pre-install
-run_old_hooks pre-install-hooks
+nix-profile-run-hooks pre-install "$new_profile" "$old_profile"
 
 echo "Installing profile" >&2
 profile_link="profile-$(profile_n)-link"
 ln -s "$new_profile" "$nix_profile_dir/$profile_link"
 ln -sfn "$profile_link" "$nix_profile_dir/profile"
 
-run_hooks post-install
-run_old_hooks post-install-hooks
+nix-profile-run-hooks post-install "$new_profile" "$old_profile"
