cec601aa5b8c2f6bb26d3825e917cf2f2bc4a333
Joshua Peek <josh@users.noreply.github.com>
2025-01-26 21:12:29 -0700

    Isolate PATH

--- pkgs/gh-random-issue.nix
+++ pkgs/gh-random-issue.nix
@@ -1,15 +1,22 @@
 {
   lib,
+  stdenv,
   writeShellApplication,
   gh,
   xdg-utils,
+  nixbits,
 }:
 writeShellApplication {
   name = "gh-random-issue";
-  runtimeInputs = [
-    gh
-    xdg-utils
-  ];
+  runtimeEnv = {
+    PATH = lib.strings.makeBinPath (
+      [
+        gh
+        xdg-utils
+      ]
+      ++ (lib.lists.optionals stdenv.hostPlatform.isDarwin [ nixbits.darwin.open ])
+    );
+  };
   text = builtins.readFile ./gh-random-issue.bash;
 
   meta = {
