d601640aca76cb1c015f79a2caec64535ef91ee4
Joshua Peek <josh@users.noreply.github.com>
2025-02-22 23:00:52 -0800

    Add dsunhook alias

--- pkgs/clean-dsstore.nix
+++ pkgs/clean-dsstore.nix
@@ -1,25 +1,27 @@
 {
   lib,
-  writeShellApplication,
-  coreutils,
+  stdenvNoCC,
+  writeShellScript,
   findutils,
 }:
-writeShellApplication {
+let
+  script = writeShellScript "clean-dsstore" ''
+    set -o errexit
+    ${findutils}/bin/find . -type f -name ".DS_Store" -print -delete
+  '';
+in
+stdenvNoCC.mkDerivation {
   name = "clean-dsstore";
 
-  runtimeEnv = {
-    PATH = lib.strings.makeBinPath [
-      coreutils
-      findutils
-    ];
-  };
-
-  text = ''
-    find . -type f -name ".DS_Store" -print -delete
+  buildCommand = ''
+    mkdir -p $out/bin
+    install -m 755 ${script} $out/bin/clean-dsstore
+    install -m 755 ${script} $out/bin/dsunhook
   '';
 
   meta = {
     description = "Clean .DS_Store files recursively";
+    mainProgram = "clean-dsstore";
     platforms = lib.platforms.darwin;
   };
 }
