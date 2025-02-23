0c25e57a2ede919279bfca04df2b38a4fb867ecd
Joshua Peek <josh@users.noreply.github.com>
2025-02-22 23:12:06 -0800

    Add external ip util

---
+++ pkgs/external-ip.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenvNoCC,
+  writeShellApplication,
+  coreutils,
+  curl,
+  gnugrep,
+}:
+let
+  script = writeShellApplication {
+    name = "external-ip";
+    runtimeEnv = {
+      PATH = lib.strings.makeBinPath [
+        curl
+        coreutils
+        gnugrep
+      ];
+    };
+    text = ''
+      curl --silent https://cloudflare.com/cdn-cgi/trace | grep ip= | cut -d= -f2
+    '';
+  };
+in
+stdenvNoCC.mkDerivation {
+  __structuredAttrs = true;
+
+  name = "external-ip";
+
+  buildCommand = ''
+    mkdir -p $out/bin
+    install -m 755 ${lib.getExe script} $out/bin/external-ip
+    install -m 755 ${lib.getExe script} $out/bin/whats-my-ip
+    install -m 755 ${lib.getExe script} $out/bin/whatsmyip
+  '';
+
+  meta = {
+    description = "Get the external IP address";
+    mainProgram = "external-ip";
+    platforms = lib.platforms.all;
+  };
+}
