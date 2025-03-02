00812e4bd7089ddef2044e3c23314f8e2e3ae328
Joshua Peek <josh@users.noreply.github.com>
2025-03-01 22:12:45 -0800

    whitespace

--- pkgs/nix-flake-all-packages-drv.nix
+++ pkgs/nix-flake-all-packages-drv.nix
@@ -12,7 +12,6 @@ writeShellApplication {
     NIX_EXPR_FILE = ./nix-flake-all-packages-drv.txt;
   };
   text = builtins.readFile ./nix-flake-all-packages-drv.bash;
-
   meta = {
     description = "Build nix derivation for all packages in current nix flake";
     platforms = lib.platforms.all;
