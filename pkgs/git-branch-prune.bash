4a87c827682897b3585a5a55361999e873b27df2
Joshua Peek <josh@users.noreply.github.com>
2025-03-03 09:38:29 -0800

    detect other default branches

--- pkgs/git-branch-prune.bash
+++ pkgs/git-branch-prune.bash
@@ -1,6 +1,17 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-git branch --merged main |
-  grep --invert-match --regexp '^[ *]*main$' --regexp "^[ *]*$current_branch$" |
+
+default_branch=""
+if git show-ref --verify --quiet refs/heads/main; then
+  default_branch="main"
+elif git show-ref --verify --quiet refs/heads/master; then
+  default_branch="master"
+else
+  echo "error: no default branch" >&2
+  exit 1
+fi
+
+git branch --merged "$default_branch" |
+  grep --invert-match --regexp "^[ *]*$default_branch$" --regexp "^[ *]*$current_branch$" |
   while read -r branch; do
     branch=${branch#"${branch%%[! ]*}"}
     branch=${branch#"*"}
