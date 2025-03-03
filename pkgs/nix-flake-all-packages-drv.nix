b9d967117af502d73e429fe15348971b606d59a3
Joshua Peek <josh@users.noreply.github.com>
2025-03-03 10:53:35 -0800

    Ensure flake source isn't needed for this file

--- pkgs/nix-flake-all-packages-drv.nix
+++ pkgs/nix-flake-all-packages-drv.nix
@@ -4,12 +4,19 @@
   writeShellApplication,
   nix,
 }:
+let
+  nix-flake-all-packages-drv = builtins.path {
+    path = ./nix-flake-all-packages-drv.txt;
+    name = "nix-flake-all-packages-drv.txt";
+    recursive = false;
+  };
+in
 writeShellApplication {
   name = "nix-flake-all-packages-drv";
   runtimeEnv = {
     PATH = lib.strings.makeBinPath [ nix ];
     SYSTEM = system;
-    NIX_EXPR_FILE = ./nix-flake-all-packages-drv.txt;
+    NIX_EXPR_FILE = nix-flake-all-packages-drv;
   };
   text = builtins.readFile ./nix-flake-all-packages-drv.bash;
   meta = {
