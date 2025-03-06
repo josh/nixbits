3f1d6250bd6c66ae7d945fd2e2cb0e12128c1f42
Joshua Peek <josh@users.noreply.github.com>
2025-03-06 12:21:26 -0800

    Add pre-wrapped age with tpm plugin

---
+++ pkgs/age-with-tpm.nix
@@ -0,0 +1,31 @@
+{
+  symlinkJoin,
+  makeWrapper,
+  age,
+  age-plugin-tpm,
+}:
+symlinkJoin {
+  pname = "age-with-tpm";
+  inherit (age) version;
+  paths = [
+    age
+    age-plugin-tpm
+  ];
+  buildInputs = [ makeWrapper ];
+  postBuild = ''
+    rm $out/bin/age
+    makeWrapper ${age}/bin/age $out/bin/age \
+      --prefix PATH : "${age-plugin-tpm}/bin"
+  '';
+
+  meta = {
+    inherit (age.meta)
+      changelog
+      description
+      homepage
+      license
+      ;
+    platforms = age-plugin-tpm.meta.platforms;
+    mainProgram = "age";
+  };
+}
