b37a3a40b3ea433382dc344db59264f324ac06c1
Joshua Peek <josh@users.noreply.github.com>
2025-03-10 15:14:12 -0700

    Add age tpm/se wrapper

---
+++ pkgs/age-with-se-tpm.nix
@@ -0,0 +1,36 @@
+{
+  symlinkJoin,
+  makeWrapper,
+  age,
+  age-plugin-tpm,
+  nur,
+}:
+let
+  age-plugin-se = nur.repos.josh.age-plugin-se;
+in
+symlinkJoin {
+  pname = "age-with-se-tpm";
+  inherit (age) version;
+  paths = [
+    age
+    age-plugin-se
+    age-plugin-tpm
+  ];
+  buildInputs = [ makeWrapper ];
+  postBuild = ''
+    rm $out/bin/age
+    makeWrapper ${age}/bin/age $out/bin/age \
+      --prefix PATH : "${age-plugin-se}/bin:${age-plugin-tpm}/bin"
+  '';
+
+  meta = {
+    inherit (age.meta)
+      changelog
+      description
+      homepage
+      license
+      ;
+    inherit (age-plugin-se.meta) platforms;
+    mainProgram = "age";
+  };
+}
