aae2aad05e5fc3e6abbf4c98de59f8c49cd62e7a
Joshua Peek <josh@users.noreply.github.com>
2025-03-12 20:34:57 -0700

    Rework windsurf cli wrapper

--- pkgs/windsurf-cli.nix
+++ pkgs/windsurf-cli.nix
@@ -8,26 +8,29 @@ let
   app = "/Applications/Windsurf.app";
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
-  name = "windsurf-impure-darwin";
-  version = "1.0.0";
+  name = "windsurf-cli";
+
+  __structuredAttrs = true;
 
   wrapper = ''
     #!${runtimeShell} -e
     exec "${app}/Contents/Resources/app/bin/windsurf" "$@"
   '';
-  passAsFile = [ "wrapper" ];
-
-  buildCommand = ''
-    mkdir -p $out/bin
-    install -m 755 $wrapperPath $out/bin/windsurf
+  preInstallHook = ''
+    #!${runtimeShell} -e
+    if [ ! -d '${app}' ]; then
+      echo "WARN: Windsurf not installed" >&2
+      echo "  https://codeium.com/download" >&2
+    fi
   '';
 
-  meta = {
-    description = "Windsurf Command Line Tools";
-    homepage = "https://codeium.com/windsurf";
-    platforms = lib.platforms.darwin;
-    mainProgram = "windsurf";
-  };
+  buildCommand = ''
+    mkdir -p $out/bin $out/share/nix/hooks/pre-install.d
+    echo "$wrapper" >$out/bin/windsurf
+    chmod +x $out/bin/windsurf
+    echo "$preInstallHook" >$out/share/nix/hooks/pre-install.d/windsurf
+    chmod +x $out/share/nix/hooks/pre-install.d/windsurf
+  '';
 
   passthru.tests =
     let
@@ -43,10 +46,15 @@ stdenvNoCC.mkDerivation (finalAttrs: {
           ''
             if [ -d '${app}' ]; then
               windsurf --version
-            else
-              echo "WARN: Windsurf not installed" >&2
             fi
             touch $out
           '';
     };
+
+  meta = {
+    description = "Windsurf Command Line Tools";
+    homepage = "https://codeium.com/windsurf";
+    platforms = lib.platforms.darwin;
+    mainProgram = "windsurf";
+  };
 })
