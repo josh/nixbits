8092d2bdf33a1d77aa037512266d613208fee67c
Joshua Peek <josh@users.noreply.github.com>
2025-03-20 15:13:22 -0700

    exec tmux

--- pkgs/tmux-attach.bash
+++ pkgs/tmux-attach.bash
@@ -2,7 +2,7 @@ sessions=()
 mapfile -t sessions < <(tmux list-sessions -F "#{session_name}" -f "#{?session_attached,0,1}" 2>/dev/null)
 
 if [ ${#sessions[@]} -gt 0 ]; then
-  tmux attach-session -t "${sessions[0]}"
+  exec tmux attach-session -t "${sessions[0]}"
 else
-  tmux new-session
+  exec tmux new-session
 fi
