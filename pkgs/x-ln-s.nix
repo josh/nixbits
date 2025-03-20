acd2615ed501b5b6a41388750e688e1bc4255d31
Joshua Peek <josh@users.noreply.github.com>
2025-03-20 10:38:16 -0700

    Add -s

--- pkgs/x-ln-s.nix
+++ pkgs/x-ln-s.nix
@@ -4,13 +4,13 @@
   coreutils,
 }:
 writeShellApplication {
-  name = "x-ln";
+  name = "x-ln-s";
   runtimeEnv = {
     "PATH" = lib.strings.makeBinPath [
       coreutils
     ];
   };
-  text = builtins.readFile ./x-ln.bash;
+  text = builtins.readFile ./x-ln-s.bash;
   meta = {
     description = "Conditionally update symlink";
     platforms = lib.platforms.all;
