c82b99548339f8f285b623ce0c29c32846d7cffd
Joshua Peek <josh@users.noreply.github.com>
2025-03-20 20:49:34 -0700

    Fix tmux leader encoding

--- pkgs/alacritty-tmux-config.nix
+++ pkgs/alacritty-tmux-config.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  stdenv,
   writers,
   nixbits,
   theme ? null,
@@ -8,140 +9,149 @@ let
   tmux = nixbits.tmux.override { inherit theme; };
   tmux-attach = nixbits.tmux-attach.override { inherit tmux; };
 
+  # C-Space
+  leader-char = "\\u0000";
+
+  macKeyboardBindings = [
+    # tmux leader alias: <leader>
+    {
+      mods = "Command";
+      key = "B";
+      chars = leader-char;
+    }
+
+    # tmux create window: <leader> c
+    {
+      mods = "Command";
+      key = "T";
+      chars = "${leader-char}c";
+    }
+
+    # tmux close window: <leader> & y
+    {
+      mods = "Command";
+      key = "W";
+      chars = "${leader-char}&y";
+    }
+
+    # tmux split horizontal: <leader> "
+    {
+      mods = "Command";
+      key = "D";
+      chars = "${leader-char}\"";
+    }
+    {
+      mods = "Option|Shift|Command";
+      key = "H";
+      chars = "${leader-char}\"";
+    }
+
+    # tmux split vertical: <leader> %
+    # {
+    #   mods = "Command";
+    #   key = "";
+    #   chars = "${leader-char}%";
+    # }
+    {
+      mods = "Option|Shift|Command";
+      key = "V";
+      chars = "${leader-char}%";
+    }
+
+    # tmux close split: <leader> x
+    {
+      mods = "Command|Shift";
+      key = "D";
+      chars = "${leader-char}x";
+    }
+
+    # tmux next window: <leader> n
+    {
+      mods = "Command|Shift";
+      key = "}";
+      chars = "${leader-char}n";
+    }
+    {
+      mods = "Control";
+      key = "Tab";
+      chars = "${leader-char}n";
+    }
+
+    # tmux previous window: <leader> p
+    {
+      mods = "Command|Shift";
+      key = "{";
+      chars = "${leader-char}p";
+    }
+    {
+      mods = "Control|Shift";
+      key = "Tab";
+      chars = "${leader-char}p";
+    }
+
+    # tmux go to window number
+    {
+      mods = "Command";
+      key = "Key1";
+      chars = "${leader-char}1";
+    }
+    {
+      mods = "Command";
+      key = "Key2";
+      chars = "${leader-char}2";
+    }
+    {
+      mods = "Command";
+      key = "Key3";
+      chars = "${leader-char}3";
+    }
+    {
+      mods = "Command";
+      key = "Key4";
+      chars = "${leader-char}4";
+    }
+    {
+      mods = "Command";
+      key = "Key5";
+      chars = "${leader-char}5";
+    }
+    {
+      mods = "Command";
+      key = "Key6";
+      chars = "${leader-char}6";
+    }
+    {
+      mods = "Command";
+      key = "Key7";
+      chars = "${leader-char}7";
+    }
+    {
+      mods = "Command";
+      key = "Key8";
+      chars = "${leader-char}8";
+    }
+    {
+      mods = "Command";
+      key = "Key9";
+      chars = "${leader-char}9";
+    }
+  ];
+
+  linuxKeyboardBindings = [
+    # TODO
+  ];
+
   config = {
     terminal.shell.program = lib.getExe tmux-attach;
-    # env.SHELL = lib.getExe zsh;
-    keyboard.bindings = [
-      # tmux leader alias: Ctrl-b
-      {
-        key = "B";
-        mods = "Command";
-        chars = "\u0000";
-      }
-
-      # tmux create window: Ctrl-b c
-      {
-        key = "T";
-        mods = "Command";
-        chars = "\u0000c";
-      }
-
-      # tmux close window: Ctrl-b & y
-      {
-        key = "W";
-        mods = "Command";
-        chars = "\u0000&y";
-      }
-
-      # tmux split horizontal: Ctrl-b "
-      {
-        key = "D";
-        mods = "Command";
-        chars = "\u0000\"";
-      }
-      {
-        key = "H";
-        mods = "Option|Shift|Command";
-        chars = "\u0000\"";
-      }
-
-      # tmux split vertical: Ctrl-b %
-      # {
-      #   key = "";
-      #   mods = "Command";
-      #   chars = "\u0000%";
-      # }
-      {
-        key = "V";
-        mods = "Option|Shift|Command";
-        chars = "\u0000%";
-      }
-
-      # tmux close split: Ctrl-b x
-      {
-        key = "D";
-        mods = "Command|Shift";
-        chars = "\u0000x";
-      }
-
-      # tmux next window: Ctrl-b n
-      {
-        key = "}";
-        mods = "Command|Shift";
-        chars = "\u0000n";
-      }
-      {
-        key = "Tab";
-        mods = "Control";
-        chars = "\u0000n";
-      }
-
-      # tmux previous window: Ctrl-b p
-      {
-        key = "{";
-        mods = "Command|Shift";
-        chars = "\u0000p";
-      }
-      {
-        key = "Tab";
-        mods = "Control|Shift";
-        chars = "\u0000p";
-      }
-
-      # tmux go to window number
-      {
-        key = "Key1";
-        mods = "Command";
-        chars = "\u00001";
-      }
-      {
-        key = "Key2";
-        mods = "Command";
-        chars = "\u00002";
-      }
-      {
-        key = "Key3";
-        mods = "Command";
-        chars = "\u00003";
-      }
-      {
-        key = "Key4";
-        mods = "Command";
-        chars = "\u00004";
-      }
-      {
-        key = "Key5";
-        mods = "Command";
-        chars = "\u00005";
-      }
-      {
-        key = "Key6";
-        mods = "Command";
-        chars = "\u00006";
-      }
-      {
-        key = "Key7";
-        mods = "Command";
-        chars = "\u00007";
-      }
-      {
-        key = "Key8";
-        mods = "Command";
-        chars = "\u00008";
-      }
-      {
-        key = "Key9";
-        mods = "Command";
-        chars = "\u00009";
-      }
-    ];
+    keyboard.bindings =
+      (lib.lists.optionals stdenv.hostPlatform.isDarwin macKeyboardBindings)
+      ++ (lib.lists.optionals stdenv.hostPlatform.isLinux linuxKeyboardBindings);
   };
 in
 (writers.writeTOML "tmux.toml" config).overrideAttrs {
   name = "alacritty-tmux-config";
   meta = {
-    description = "Alacritty macOS tmux config";
-    # TODO: Add linux keybindings
-    platforms = lib.platforms.darwin;
+    description = "Alacritty tmux config";
+    platforms = lib.platforms.all;
   };
 }
