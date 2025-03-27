2ca5fad58f097fba276f928678508b16f7b0431b
Joshua Peek <josh@users.noreply.github.com>
2025-03-26 21:28:27 -0700

    Drop code sign stuff, always run

--- pkgs/install-mac-app.bash
+++ pkgs/install-mac-app.bash
@@ -3,7 +3,7 @@ usage() {
 }
 
 dry_run=false
-nix_store_path=""
+src=""
 app_dir="/Applications"
 
 while [ $# -gt 0 ]; do
@@ -29,78 +29,70 @@ while [ $# -gt 0 ]; do
     exit 1
     ;;
   *)
-    if [ -n "$nix_store_path" ]; then
+    if [ -n "$src" ]; then
       echo "error: too many arguments" >&2
       exit 1
     fi
-    nix_store_path="$1"
+    src="$1"
     shift
     ;;
   esac
 done
 
-if [ -z "$nix_store_path" ]; then
-  echo "error: missing NIX_STORE_PATH argument" >&2
+if [ ! -d "$app_dir" ]; then
+  echo "error: $app_dir not a directory" >&2
   exit 1
 fi
 
-if [ ! -d "$nix_store_path" ]; then
+if [ -z "$src" ]; then
+  echo "error: missing source App" >&2
+  exit 1
+fi
+
+if [ ! -d "$src" ]; then
   echo "error: src is not a directory" >&2
   exit 1
 fi
 
-if [[ $nix_store_path != /nix/store/* ]]; then
+if [[ $src != /nix/store/* ]]; then
   echo "error: src must be a directory under /nix/store" >&2
   exit 1
 fi
 
-apps=("$nix_store_path/Applications/"*.app)
+if [[ $src != *.app ]]; then
+  apps=("$src/Applications/"*.app)
 
-if [ ${#apps[@]} -eq 0 ]; then
-  echo "error: no .app found in $nix_store_path/Applications/" >&2
-  exit 1
+  if [ ${#apps[@]} -eq 0 ]; then
+    echo "error: no .app found in $src/Applications/" >&2
+    exit 1
+  elif [ ${#apps[@]} -gt 1 ]; then
+    echo "error: multiple .app found in $src/Applications/" >&2
+    exit 1
+  fi
+
+  src="${apps[0]}"
 fi
 
-if [ ${#apps[@]} -gt 1 ]; then
-  echo "error: multiple .app found in $nix_store_path/Applications/" >&2
-  for app in "${apps[@]}"; do
-    echo "  - $(basename "$app")" >&2
-  done
-  exit 1
-fi
-
-src="${apps[0]}/"
 app_name="$(basename "$src")"
 dst="$app_dir/$app_name/"
 
-cdhash() {
-  codesign --verbose=3 --display "$1" 2>&1 |
-    grep --only-matching 'CDHash=\S*' |
-    cut --delimiter='=' --fields=2
+_has_changes() {
+  grep --quiet --invert-match 'Number of regular files transferred: 0'
 }
 
-src_hash="$(cdhash "$src")"
-dst_hash=""
-
-if [ -d "$dst" ]; then
-  dst_hash="$(cdhash "$dst" || echo "invalid")"
-fi
-
-if [ "$src_hash" = "$dst_hash" ]; then
-  exit 0
-fi
-
-if [ "$dry_run" = true ]; then
-  echo "Would install $src to $app_dir/" >&2
-  exit 0
-fi
-
-x() {
-  echo "+" "$@" >&2
-  "$@"
+_rsync() {
+  if [ "$dry_run" = true ]; then
+    if rsync --dry-run --stats "$@" | _has_changes; then
+      echo "dry-run: + rsync" "$@" >&2
+    fi
+  else
+    if rsync --stats "$@" | _has_changes; then
+      echo "+ rsync" "$@" >&2
+    fi
+  fi
 }
 
-x rsync \
+_rsync \
   --archive \
   --checksum \
   --chmod=-w \
@@ -108,6 +100,5 @@ x rsync \
   --delete \
   --no-group \
   --no-owner \
-  --verbose \
   "$src" \
   "$dst"
--- pkgs/install-mac-app.nix
+++ pkgs/install-mac-app.nix
@@ -1,24 +1,58 @@
 {
   lib,
   writeShellApplication,
+  runCommand,
   coreutils,
   gnugrep,
   rsync,
-  nixbits,
+  neovide,
 }:
-writeShellApplication {
-  name = "install-mac-app";
-  runtimeEnv = {
-    PATH = lib.strings.makeBinPath [
-      coreutils
-      gnugrep
-      rsync
-      nixbits.darwin.codesign
-    ];
+let
+  script = writeShellApplication {
+    name = "install-mac-app";
+    runtimeEnv = {
+      PATH = lib.strings.makeBinPath [
+        coreutils
+        gnugrep
+        rsync
+      ];
+    };
+    text = builtins.readFile ./install-mac-app.bash;
+    meta = {
+      description = "Copy macOS App into /Applications";
+      platforms = lib.platforms.darwin;
+    };
   };
-  text = builtins.readFile ./install-mac-app.bash;
-  meta = {
-    description = "Copy macOS App into /Applications";
-    platforms = lib.platforms.darwin;
+in
+script.overrideAttrs (_finalAttrs: {
+  passthru.tests = {
+    install-dry-run = runCommand "test-install-dry-run" { nativeBuildInputs = [ script ]; } ''
+      mkdir -p "$TMPDIR/Applications"
+      install-mac-app --dry-run --appdir "$TMPDIR/Applications" ${neovide}
+      [ ! -d "$TMPDIR/Applications/Neovide.app" ]
+      touch $out
+    '';
+
+    install-drv = runCommand "test-install-drv" { nativeBuildInputs = [ script ]; } ''
+      mkdir -p "$TMPDIR/Applications"
+      install-mac-app --appdir "$TMPDIR/Applications" ${neovide}
+      [ -d "$TMPDIR/Applications/Neovide.app" ]
+      touch $out
+    '';
+
+    install-app = runCommand "test-install-app" { nativeBuildInputs = [ script ]; } ''
+      mkdir -p "$TMPDIR/Applications"
+      install-mac-app --appdir "$TMPDIR/Applications" ${neovide}/Applications/Neovide.app
+      [ -d "$TMPDIR/Applications/Neovide.app" ]
+      touch $out
+    '';
+
+    install-twice = runCommand "test-install-twice" { nativeBuildInputs = [ script ]; } ''
+      mkdir -p "$TMPDIR/Applications"
+      install-mac-app --appdir "$TMPDIR/Applications" ${neovide}
+      [ -d "$TMPDIR/Applications/Neovide.app" ]
+      install-mac-app --appdir "$TMPDIR/Applications" ${neovide}
+      touch $out
+    '';
   };
-}
+})
