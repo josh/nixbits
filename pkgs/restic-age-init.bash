fd99c73c0664c831dc5dc1bbfed0a534954257d4
Joshua Peek <josh@users.noreply.github.com>
2025-03-29 12:27:24 -0700

    Just allow a recipient

--- pkgs/restic-age-init.bash
+++ pkgs/restic-age-init.bash
@@ -1,8 +1,9 @@
 RESTIC_REPOSITORY="${RESTIC_REPOSITORY:-}"
 unset RESTIC_PASSWORD_FILE
 unset RESTIC_PASSWORD_COMMAND
-RESTIC_AGE_IDENTITY_FILE="${RESTIC_AGE_IDENTITY_FILE:-}"
 RESTIC_AGE_IDENTITY_COMMAND="${RESTIC_AGE_IDENTITY_COMMAND:-}"
+RESTIC_AGE_IDENTITY_FILE="${RESTIC_AGE_IDENTITY_FILE:-}"
+RESTIC_AGE_RECIPIENT="${RESTIC_AGE_RECIPIENT:-}"
 
 restic_init_args=()
 while [[ $# -gt 0 ]]; do
@@ -22,8 +23,9 @@ while [[ $# -gt 0 ]]; do
     exit 1
     ;;
   --recipient)
-    echo "error: --recipient is extracted from --identity-file|command" >&2
-    exit 1
+    RESTIC_AGE_RECIPIENT="$2"
+    export RESTIC_AGE_RECIPIENT
+    shift 2
     ;;
   --identity-file)
     RESTIC_AGE_IDENTITY_FILE="$2"
@@ -56,14 +58,15 @@ export RESTIC_PASSWORD_FILE
 x restic init "${restic_init_args[@]}"
 orig_key_id=$(restic key list --json | jq --raw-output 'map(.id)[0]')
 
-recipient=""
-if [ -n "$RESTIC_AGE_IDENTITY_FILE" ]; then
-  recipient=$(age-keygen -y "$RESTIC_AGE_IDENTITY_FILE")
-elif [ -n "$RESTIC_AGE_IDENTITY_COMMAND" ]; then
-  recipient=$(eval "$RESTIC_AGE_IDENTITY_COMMAND" | age-keygen -y)
+if [ -z "$RESTIC_AGE_RECIPIENT" ]; then
+  if [ -n "$RESTIC_AGE_IDENTITY_FILE" ]; then
+    RESTIC_AGE_RECIPIENT=$(age-keygen -y "$RESTIC_AGE_IDENTITY_FILE")
+  elif [ -n "$RESTIC_AGE_IDENTITY_COMMAND" ]; then
+    RESTIC_AGE_RECIPIENT=$(eval "$RESTIC_AGE_IDENTITY_COMMAND" | age-keygen -y)
+  fi
 fi
 
-x restic-age-key add --recipient "$recipient"
+x restic-age-key add --recipient "$RESTIC_AGE_RECIPIENT"
 restic-age-key password >"$RESTIC_PASSWORD_FILE"
 
 x restic key remove "$orig_key_id"
