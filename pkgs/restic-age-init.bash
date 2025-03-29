8203843b4af1afac40fb3ffe98b40d666f6fbeef
Joshua Peek <josh@users.noreply.github.com>
2025-03-29 10:56:32 -0700

    Add restic age init helper

---
+++ pkgs/restic-age-init.bash
@@ -0,0 +1,68 @@
+RESTIC_REPOSITORY="${RESTIC_REPOSITORY:-}"
+unset RESTIC_PASSWORD_FILE
+unset RESTIC_PASSWORD_COMMAND
+RESTIC_AGE_IDENTITY_FILE="${RESTIC_AGE_IDENTITY_FILE:-}"
+RESTIC_AGE_IDENTITY_COMMAND="${RESTIC_AGE_IDENTITY_COMMAND:-}"
+
+restic_init_args=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --repo)
+    RESTIC_REPOSITORY="$2"
+    export RESTIC_REPOSITORY
+    restic_init_args+=("--repo" "$2")
+    shift 2
+    ;;
+  --password-file)
+    echo "error: --password-file is not supported" >&2
+    exit 1
+    ;;
+  --password-command)
+    echo "error: --password-command is not supported" >&2
+    exit 1
+    ;;
+  --recipient)
+    echo "error: --recipient is extracted from --identity-file|command" >&2
+    exit 1
+    ;;
+  --identity-file)
+    RESTIC_AGE_IDENTITY_FILE="$2"
+    export RESTIC_AGE_IDENTITY_FILE
+    shift 2
+    ;;
+  --identity-command)
+    RESTIC_AGE_IDENTITY_COMMAND="$2"
+    export RESTIC_AGE_IDENTITY_COMMAND
+    shift 2
+    ;;
+  *)
+    restic_init_args+=("$1")
+    shift
+    ;;
+  esac
+done
+
+x() {
+  echo "+" "$@" >&2
+  "$@"
+}
+
+RESTIC_PASSWORD_FILE=$(mktemp)
+openssl rand -base64 32 >"$RESTIC_PASSWORD_FILE"
+export RESTIC_PASSWORD_FILE
+
+x restic init "${restic_init_args[@]}"
+orig_key_id=$(restic key list --json | jq --raw-output 'map(.id)[0]')
+
+recipient=""
+if [ -n "$RESTIC_AGE_IDENTITY_FILE" ]; then
+  recipient=$(age-keygen -y "$RESTIC_AGE_IDENTITY_FILE")
+elif [ -n "$RESTIC_AGE_IDENTITY_COMMAND" ]; then
+  recipient=$(eval "$RESTIC_AGE_IDENTITY_COMMAND" | age-keygen -y)
+fi
+
+x restic-age-key add --recipient "$recipient"
+restic-age-key password >"$RESTIC_PASSWORD_FILE"
+
+x restic key remove "$orig_key_id"
+rm -f "$RESTIC_PASSWORD_FILE"
---
+++ pkgs/restic-age-init.nix
@@ -0,0 +1,91 @@
+{
+  lib,
+  writeShellApplication,
+  runCommand,
+  age,
+  coreutils,
+  jq,
+  openssl,
+  restic,
+  nur,
+}:
+let
+  restic-age-init = writeShellApplication {
+    name = "restic-age-init";
+    runtimeEnv = {
+      PATH = lib.strings.makeBinPath [
+        age
+        coreutils
+        jq
+        openssl
+        restic
+        nur.repos.josh.restic-age-key
+      ];
+    };
+    text = builtins.readFile ./restic-age-init.bash;
+    meta = {
+      description = "Initialize restic repository with age key";
+      platforms = lib.platforms.all;
+    };
+  };
+in
+restic-age-init.overrideAttrs (
+  finalAttrs: _previousAttrs: {
+    passthru.tests =
+      let
+        restic-age-init = finalAttrs.finalPackage;
+      in
+      {
+        init-identity-file =
+          runCommand "test-init-identity-file"
+            {
+              nativeBuildInputs = [
+                age
+                restic-age-init
+              ];
+            }
+            ''
+              age-keygen --output key.txt
+              restic-age-init --repo "$TMPDIR/restic-repo" \
+                --identity-file key.txt
+              touch $out
+            '';
+
+        init-identity-command =
+          runCommand "test-init-identity-command"
+            {
+              nativeBuildInputs = [
+                age
+                restic-age-init
+              ];
+            }
+            ''
+              age-keygen --output key.txt
+              restic-age-init --repo "$TMPDIR/restic-repo" \
+                --identity-command 'cat key.txt'
+              touch $out
+            '';
+
+        init-from-repo =
+          runCommand "test-init-from-repo"
+            {
+              nativeBuildInputs = [
+                age
+                restic
+                restic-age-init
+              ];
+            }
+            ''
+              echo "secret" >password.txt
+              restic init --repo "$TMPDIR/restic-repo-a" --password-file "password.txt"
+              age-keygen --output key.txt
+              restic-age-init --repo "$TMPDIR/restic-repo-b" \
+                --identity-file key.txt \
+                --from-repo "$TMPDIR/restic-repo-a" \
+                --from-password-file "password.txt" \
+                --copy-chunker-params
+              touch $out
+            '';
+      };
+  }
+)
