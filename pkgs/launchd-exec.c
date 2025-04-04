e7d95ee79e8d27f0dac5491472d97e9ccc353b7d
Joshua Peek <josh@users.noreply.github.com>
2025-04-04 15:28:56 -0700

    Add version to launchd-exec wrapper

--- pkgs/launchd-exec.c
+++ pkgs/launchd-exec.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+static const char *version = "0.1.0";
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    fprintf(stderr, "usage: %s /nix/store/<hash>-<program>\n", argv[0]);
+    fprintf(stderr, "usage: %s <program>\n", argv[0]);
+    return 1;
+  }
+
+  if (strcmp(argv[1], "-V") == 0 || strcmp(argv[1], "--version") == 0) {
+    printf("%s\n", version);
+    return 0;
+  }
+
+  if (getppid() != 1) {
+    fprintf(stderr, "error: must be a child of launchd\n");
     return 1;
   }
 
