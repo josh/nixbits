301a928895a51f1170d31dc69316568960d8c4bf
Joshua Peek <josh@users.noreply.github.com>
2025-04-04 16:04:13 -0700

    Check system and user policy on install

--- pkgs/launchd-exec-install.nix
+++ pkgs/launchd-exec-install.nix
@@ -8,13 +8,17 @@
   nixbits,
 }:
 let
-  tccpolicy = {
+  inherit (nur.repos.josh) tccpolicy;
+  inherit (nixbits) launchctl-spawn;
+
+  tcc-system-policy = writers.writeJSON "launch-exec-tccpolicy.json" {
     "SystemPolicyAllFiles" = true;
+  };
+  tcc-user-policy = writers.writeJSON "launch-exec-tccpolicy.json" {
     "AppleEvents" = [
       "com.apple.systemevents"
     ];
   };
-  tccpolicy-file = writers.writeJSON "launch-exec-tccpolicy.json" tccpolicy;
 in
 writeShellApplication {
   name = "launchd-exec-install";
@@ -22,12 +26,14 @@ writeShellApplication {
     PATH = lib.strings.makeBinPath [
       coreutils
       darwin.sudo
+      launchctl-spawn
       nixbits.darwin.open
-      nur.repos.josh.tccpolicy
+      tccpolicy
     ];
     INSTALL_PATH = "/usr/local/bin/launchd-exec";
     INSTALL_DIR = "/usr/local/bin";
-    POLICY_FILE = tccpolicy-file;
+    SYSTEM_POLICY_FILE = tcc-system-policy;
+    USER_POLICY_FILE = tcc-user-policy;
     VERSION = nixbits.launchd-exec.version;
   };
   text = ''
@@ -42,9 +48,14 @@ writeShellApplication {
       x sudo ${coreutils}/bin/install -m 755 ${nixbits.launchd-exec}/bin/launchd-exec "$INSTALL_PATH"
     fi
 
-    if ! x-silent tccpolicy check --client "$INSTALL_PATH" --policy "$POLICY_FILE"; then
+    if ! x-silent tccpolicy check --client "$INSTALL_PATH" --policy "$SYSTEM_POLICY_FILE"; then
       open "$INSTALL_DIR"
       open "x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles"
+      exit 1
+    fi
+
+    if ! x-silent tccpolicy check --client "$INSTALL_PATH" --policy "$USER_POLICY_FILE"; then
+      x launchctl-spawn -- "$INSTALL_PATH" ${tccpolicy}/bin/tccpolicy request --policy "$USER_POLICY_FILE"
       exit 1
     fi
   '';
