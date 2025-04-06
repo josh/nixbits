432b43311a29fae990fcff33ffc67bcd4db5fa15
Joshua Peek <josh@users.noreply.github.com>
2025-04-05 17:23:43 -0700

    Improve error handling

--- pkgs/ensure-newline.c
+++ pkgs/ensure-newline.c
@@ -9,14 +9,18 @@ int main() {
   char last_byte = '\0';
 
   while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
-    write(STDOUT_FILENO, buffer, bytes_read);
+    if (write(STDOUT_FILENO, buffer, bytes_read) < 0) {
+      return 1;
+    }
     if (bytes_read > 0) {
       last_byte = buffer[bytes_read - 1];
     }
   }
 
   if (last_byte != '\n') {
-    write(STDOUT_FILENO, "\n", 1);
+    if (write(STDOUT_FILENO, "\n", 1) < 0) {
+      return 1;
+    }
   }
 
   return 0;
