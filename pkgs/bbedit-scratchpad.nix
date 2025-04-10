f886f19f252c4d7557e0c2e575396a29fdd9018f
Joshua Peek <josh@users.noreply.github.com>
2025-04-09 19:21:10 -0700

    Move script to seperate file

--- pkgs/bbedit-scratchpad.nix
+++ pkgs/bbedit-scratchpad.nix
@@ -1,28 +1,15 @@
 {
   lib,
-  writeTextFile,
   writeShellApplication,
   nixbits,
 }:
-let
-  scpt = writeTextFile {
-    name = "bbedit-scratchpad.scpt";
-    text = ''
-      tell application "BBEdit"
-        activate
-        open scratchpad document
-        set the index of the window of scratchpad document to 1
-      end tell
-    '';
-  };
-in
 writeShellApplication {
   name = "bbedit-scratchpad";
   runtimeEnv = {
     PATH = lib.strings.makeBinPath [ nixbits.darwin.osascript ];
   };
   text = ''
-    osascript <${scpt}
+    osascript <${./bbedit-scratchpad.applescript}
   '';
   meta = {
     description = "Open BBEdit's scratchpad document";
