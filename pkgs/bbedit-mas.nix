3d7820eb3634dcad099005893e832fa24e502430
Joshua Peek <josh@users.noreply.github.com>
2025-05-28 15:19:30 -0700

    Write out tccpolicy

--- pkgs/bbedit-mas.nix
+++ pkgs/bbedit-mas.nix
@@ -1,34 +1,57 @@
 {
   lib,
   stdenvNoCC,
-  writeShellScript,
   runCommandLocal,
+  jq,
 }:
 let
   app = "/Applications/BBEdit.app";
-  bbeditHelper =
-    name:
-    writeShellScript name ''
-      exec "${app}/Contents/Helpers/${name}" "$@"
-    '';
-  preinstallHook = writeShellScript "bbedit-mas-preinstall-hook" ''
+in
+stdenvNoCC.mkDerivation (finalAttrs: {
+  name = "bbedit-mas";
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [ jq ];
+
+  appPath = "/Applications/BBEdit.app";
+
+  masID = 404009241;
+
+  tccpolicyPolicy = {
+    "com.barebones.bbedit" = {
+      "SystemPolicyAllFiles" = true;
+    };
+  };
+
+  preinstallHookScript = ''
     if [ ! -d "/Applications/BBEdit.app" ]; then
       echo "warn: BBEdit is not installed" >&2
       echo "  https://www.barebones.com/products/bbedit/" >&2
     fi
   '';
-in
-stdenvNoCC.mkDerivation (finalAttrs: {
-  name = "bbedit-mas";
 
   buildCommand = ''
-    mkdir -p $out/bin $out/share/nix/hooks/pre-install.d $out/share/mas
-    install -m 755 ${bbeditHelper "bbedit_tool"} $out/bin/bbedit
-    install -m 755 ${bbeditHelper "bbdiff"} $out/bin/bbdiff
-    install -m 755 ${bbeditHelper "bbfind"} $out/bin/bbfind
-    install -m 755 ${bbeditHelper "bbresults"} $out/bin/bbresults
-    install -m 755 ${preinstallHook} $out/share/nix/hooks/pre-install.d/bbedit
-    echo "BBEdit" >$out/share/mas/404009241
+    mkShellScript() {
+      echo "#!$shell" >"$2"
+      echo -n "$1" >>"$2"
+      chmod +x "$2"
+    }
+
+    mkdir -p $out/bin
+    mkShellScript "exec \"$appPath/Contents/Helpers/bbedit_tool\" \"\$@\"" "$out/bin/bbedit"
+    mkShellScript "exec \"$appPath/Contents/Helpers/bbdiff\" \"\$@\"" "$out/bin/bbdiff"
+    mkShellScript "exec \"$appPath/Contents/Helpers/bbfind\" \"\$@\"" "$out/bin/bbfind"
+    mkShellScript "exec \"$appPath/Contents/Helpers/bbresults\" \"\$@\"" "$out/bin/bbresults"
+
+    mkdir -p $out/share/nix/hooks/pre-install.d
+    mkShellScript "$preinstallHookScript" "$out/share/nix/hooks/pre-install.d/bbedit"
+
+    mkdir -p $out/share/mas
+    echo "BBEdit" >$out/share/mas/$masID
+
+    mkdir -p $out/share/tccpolicy.d
+    cat "$NIX_ATTRS_JSON_FILE" | jq --raw-output '.tccpolicyPolicy' >$out/share/tccpolicy.d/bbedit.json
   '';
 
   passthru.tests =
