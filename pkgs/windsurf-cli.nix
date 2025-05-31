2bad65bdd8e77d2051da2b9a48ab5c52e6112b8d
Joshua Peek <josh@users.noreply.github.com>
2025-05-31 15:19:51 -0700

    Add tccpolicy

--- pkgs/windsurf-cli.nix
+++ pkgs/windsurf-cli.nix
@@ -3,6 +3,7 @@
   stdenvNoCC,
   runtimeShell,
   runCommandLocal,
+  jq,
 }:
 let
   app = "/Applications/Windsurf.app";
@@ -16,6 +17,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     #!${runtimeShell} -e
     exec "${app}/Contents/Resources/app/bin/windsurf" "$@"
   '';
+
   preInstallHook = ''
     #!${runtimeShell} -e
     if [ ! -d '${app}' ]; then
@@ -24,12 +26,24 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     fi
   '';
 
+  tccpolicyPolicy = {
+    "com.exafunction.windsurf" = {
+      "SystemPolicyAllFiles" = true;
+    };
+  };
+
+  nativeBuildInputs = [ jq ];
+
   buildCommand = ''
     mkdir -p $out/bin $out/share/nix/hooks/pre-install.d
     echo "$wrapper" >$out/bin/windsurf
     chmod +x $out/bin/windsurf
+
     echo "$preInstallHook" >$out/share/nix/hooks/pre-install.d/windsurf
     chmod +x $out/share/nix/hooks/pre-install.d/windsurf
+
+    mkdir -p $out/share/tccpolicy.d
+    cat "$NIX_ATTRS_JSON_FILE" | jq --raw-output '.tccpolicyPolicy' >$out/share/tccpolicy.d/windsurf.json
   '';
 
   passthru.tests =
