6473adf3bfc4b48c3c2424dc968b6858b2b8c1ec
Joshua Peek <josh@users.noreply.github.com>
2025-06-08 12:55:07 -0700

    Add grep with colors

---
+++ pkgs/grep.nix
@@ -0,0 +1,36 @@
+{
+  stdenvNoCC,
+  makeWrapper,
+  lndir,
+  gnugrep,
+}:
+stdenvNoCC.mkDerivation {
+  name = "grep";
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+  makeWrapperArgs =
+    [
+      "--set"
+      "GREP_COLORS"
+      "mt=1;32"
+    ]
+    ++ [
+      "--add-flags"
+      "--color=auto"
+    ];
+
+  buildCommand = ''
+    mkdir $out
+    ${lndir}/bin/lndir -silent ${gnugrep} $out
+
+    rm $out/bin/grep
+    makeWrapper ${gnugrep}/bin/grep $out/bin/grep "''${makeWrapperArgs[@]}"
+  '';
+
+  meta = {
+    description = "grep with colors";
+    mainProgram = "grep";
+  };
+}
