e2a1f7342cff3b096d01a7472326993e80dd15a3
Joshua Peek <josh@users.noreply.github.com>
2025-06-14 16:58:38 -0700

    Add some macos defaults config

--- pkgs/tailscale-mas.nix
+++ pkgs/tailscale-mas.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  formats,
   stdenvNoCC,
   writeShellScript,
 }:
@@ -12,15 +13,27 @@ let
       echo "warn: Tailscale is not installed" >&2
     fi
   '';
+
+  yaml = formats.yaml { };
+  defaultsConfig = yaml.generate "tailscale.yml" {
+    description = "Tailscale";
+    kill = [ "Tailscale" ];
+    data = {
+      "io.tailscale.ipn.macos" = {
+        FileSharingConfiguration = "show";
+      };
+    };
+  };
 in
 stdenvNoCC.mkDerivation {
   name = "tailscale-mas";
 
   buildCommand = ''
-    mkdir -p $out/bin $out/share/mas $out/share/nix/hooks/pre-install.d
+    mkdir -p $out/bin $out/share/mas $out/share/nix/hooks/pre-install.d $out/share/defaults.d
     install -m 755 ${wrapper} $out/bin/tailscale
     install -m 755 ${preinstallHook} $out/share/nix/hooks/pre-install.d/tailscale-mas
     echo "Tailscale" >$out/share/mas/1475387142
+    cp ${defaultsConfig} $out/share/defaults.d/tailscale.yml
   '';
 
   meta = {
