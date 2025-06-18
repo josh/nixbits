99cf48c63195459e096e7ad3ae69cd1840569585
Joshua Peek <josh@users.noreply.github.com>
2025-06-18 10:49:06 -0700

    Improve setting on existing repo

--- pkgs/jujutsu-git-set-upstream.bash
+++ pkgs/jujutsu-git-set-upstream.bash
@@ -2,7 +2,6 @@
 source "$XTRACE_PATH/share/bash/xtrace.bash"
 
 origin_url=$(jj --ignore-working-copy git remote list | grep '^origin ' | cut -d' ' -f2)
-upstream_url=$(jj --ignore-working-copy git remote list | grep '^upstream ' | cut -d' ' -f2 || true)
 
 is_fork=$(gh repo view "$origin_url" --json 'isFork' --jq '.isFork')
 if [ "$is_fork" = "false" ]; then
@@ -10,13 +9,15 @@ if [ "$is_fork" = "false" ]; then
   exit 1
 fi
 
+orig_upstream_url=$(jj --ignore-working-copy git remote list | grep '^upstream ' | cut -d' ' -f2 || true)
 upstream_url=$(gh repo view "$origin_url" --json 'parent' --jq '"https://github.com/\(.parent.owner.login)/\(.parent.name).git"')
 upstream_branch=$(gh repo view "$upstream_url" --json 'defaultBranchRef' --jq '.defaultBranchRef.name')
 
-if [ -n "$upstream_url" ]; then
-  x jj git remote set-url upstream "$upstream_url"
-else
+if [ -z "$orig_upstream_url" ]; then
   x jj git remote add upstream "$upstream_url"
+elif [ "$orig_upstream_url" != "$upstream_url" ]; then
+  x jj git remote set-url upstream "$upstream_url"
 fi
 
+x jj git fetch --remote upstream
 x jj config set --repo 'revset-aliases."trunk()"' "$upstream_branch@upstream"
