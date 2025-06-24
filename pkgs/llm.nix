8919779c370ff3dbd440720fb27227e96dba4ba7
Joshua Peek <josh@users.noreply.github.com>
2025-06-24 10:12:54 -0700

    Disable problematic llm-sentence-transformers

--- pkgs/llm.nix
+++ pkgs/llm.nix
@@ -8,5 +8,5 @@ llm.override {
   enable-llm-jq = true;
   enable-llm-ollama = true;
   enable-llm-openai-plugin = true;
-  enable-llm-sentence-transformers = true;
+  # enable-llm-sentence-transformers = true;
 }
