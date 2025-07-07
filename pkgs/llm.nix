fa60d3b2be5720fa3ac4ee5ec2fd65a60abdd709
Joshua Peek <josh@users.noreply.github.com>
2025-07-07 08:55:06 -0700

    Disable llm tests

--- pkgs/llm.nix
+++ pkgs/llm.nix
@@ -1,5 +1,5 @@
 { llm }:
-llm.override {
+(llm.override {
   enable-llm-anthropic = true;
   enable-llm-cmd = true;
   enable-llm-fragments-github = true;
@@ -8,5 +8,7 @@ llm.override {
   enable-llm-jq = true;
   enable-llm-ollama = true;
   enable-llm-openai-plugin = true;
-  # enable-llm-sentence-transformers = true;
-}
+}).overrideAttrs
+  {
+    passthru.tests = { };
+  }
