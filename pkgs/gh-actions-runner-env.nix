b8bbb94f040511594dcb775e0378182f636f2cff
Joshua Peek <josh@users.noreply.github.com>
2025-07-14 17:23:42 -0700

    Add gh actions common workflow deps

---
+++ pkgs/gh-actions-runner-env.nix
@@ -0,0 +1,35 @@
+{
+  buildEnv,
+  nixbits,
+  # keep-sorted start
+  bash,
+  coreutils,
+  gh,
+  git,
+  gnutar,
+  gzip,
+  jq,
+  nix,
+  which,
+# keep-sorted end
+}:
+let
+  inherit (nixbits) gtar;
+in
+buildEnv {
+  name = "gh-actions-runner-env";
+  paths = [
+    # keep-sorted start
+    bash
+    coreutils
+    gh
+    git
+    gnutar
+    gtar
+    gzip
+    jq
+    nix
+    which
+    # keep-sorted end
+  ];
+}
