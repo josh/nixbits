c5515da69bae72d338a771999b10076fcdbf33ee
Joshua Peek <josh@users.noreply.github.com>
2025-07-23 20:23:39 -0700

    Add dry-run support

--- pkgs/x-cp.bash
+++ pkgs/x-cp.bash
@@ -2,9 +2,23 @@
 source "$XTRACE_PATH/share/bash/xtrace.bash"
 
 if [ $# -lt 2 ]; then
-  echo "usage: x-cp <src> <dst>" >&2
+  echo "usage: x-cp [--dry-run] <src> <dst>" >&2
   exit 1
 fi
+
+dry_run=false
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --dry-run)
+    dry_run=true
+    shift
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
 
 src="$1"
 dst="$2"
@@ -20,10 +34,14 @@ fi
 
 parent_dir="$(dirname "$dst")"
 if [ ! -d "$parent_dir" ]; then
-  x mkdir -p "$parent_dir"
+  x-dry-run $dry_run -- mkdir -p "$parent_dir"
 fi
 
-x cp "$src" "$dst"
+x-dry-run $dry_run -- cp "$src" "$dst"
+
+if [ -n "$dry_run" ]; then
+  exit 0
+fi
 
 if [ ! -f "$dst" ] || ! cmp -s "$src" "$dst"; then
   echo "error: failed to copy file" >&2
