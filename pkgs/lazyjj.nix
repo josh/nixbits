45d13ce52bdff6bf082f339bca630e06d95819be
Joshua Peek <josh@users.noreply.github.com>
2025-07-26 08:59:03 -0700

    Seperate lazyjj wrapper from upstream drv

--- pkgs/lazyjj.nix
+++ pkgs/lazyjj.nix
@@ -1,16 +1,53 @@
 {
   lib,
+  stdenvNoCC,
+  makeWrapper,
+  lndir,
   jujutsu,
   lazyjj,
   nixbits,
 }:
-lazyjj.overrideAttrs (
-  _finalAttrs: _previousAttrs: {
-    postInstall = ''
-      wrapProgram $out/bin/lazyjj \
-        --prefix PATH : ${lib.strings.makeBinPath [ jujutsu ]} \
-        --set JJ_CONFIG ${nixbits.jujutsu-config} \
-        --set XDG_CONFIG_HOME ${nixbits.jujutsu-xdg-config-home}
-    '';
-  }
-)
+let
+  lazyjj' = lazyjj.overrideAttrs {
+    # Disable wrapProgram
+    postInstall = "";
+  };
+in
+stdenvNoCC.mkDerivation {
+  name = lazyjj'.name;
+  pname = lazyjj'.pname;
+  version = lazyjj'.version;
+  meta = lazyjj'.meta;
+
+  __structuredAttrs = true;
+
+  makeWrapperArgs = [
+    "--prefix"
+    "PATH"
+    ":"
+    (lib.strings.makeBinPath [ jujutsu ])
+  ]
+  ++ [
+    "--set"
+    "JJ_CONFIG"
+    "${nixbits.jujutsu-config}"
+  ]
+  ++ [
+    "--set"
+    "XDG_CONFIG_HOME"
+    "${nixbits.jujutsu-xdg-config-home}"
+  ];
+
+  nativeBuildInputs = [
+    makeWrapper
+    lndir
+  ];
+
+  buildCommand = ''
+    mkdir $out
+    lndir -silent ${lazyjj'} $out
+
+    rm $out/bin/lazyjj
+    makeWrapper ${lazyjj'}/bin/lazyjj $out/bin/lazyjj "''${makeWrapperArgs[@]}"
+  '';
+}
