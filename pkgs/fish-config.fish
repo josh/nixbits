119a2b769fede75b26dcf677c0952657a03a2032
Joshua Peek <josh@users.noreply.github.com>
2025-08-14 14:09:51 -0700

    Ensure xdg nix profile gets in fish path

--- pkgs/fish-config.fish
+++ pkgs/fish-config.fish
@@ -2,13 +2,21 @@ if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
   source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
 end
 
+if test -d "/nix/var/nix/profiles/default/bin"
+  fish_add_path --prepend --global "/nix/var/nix/profiles/default/bin"
+end
+if test -d "$HOME/.local/state/nix/profile/bin"
+  fish_add_path --prepend --global "$HOME/.local/state/nix/profile/bin"
+else if test -d "$HOME/.nix-profile/bin"
+  fish_add_path --prepend --global "$HOME/.nix-profile/bin"
+end
+fish_add_path --prepend --global @fish-path@/bin
+
 for file in @out@/conf.d/*.fish
   source $file
 end
 
 set --global fish_greeting
-
-set --export PATH $PATH @fish-path@/bin
 
 status is-login; and begin
   # Login shell initialization
