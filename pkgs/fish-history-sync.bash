0de5107b12144443f1153ebe670f3a1bf28ec722
Joshua Peek <josh@users.noreply.github.com>
2025-08-17 17:13:52 -0700

    Enable fish bidirectional sync

--- pkgs/fish-history-sync.bash
+++ pkgs/fish-history-sync.bash
@@ -9,19 +9,19 @@ CLOUD_HISTFILES="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Terminal/his
 LOCAL_HISTFILE="$HOME/.local/share/fish/fish_history"
 HOSTNAME=$(hostname -s | tr '[:upper:]' '[:lower:]')
 CLOUD_HISTFILE="$CLOUD_HISTFILES/$HOSTNAME.fish-history"
-# LOCAL_COUNT=$(wc -l <"$LOCAL_HISTFILE")
+LOCAL_COUNT=$(wc -l <"$LOCAL_HISTFILE")
 
 sponge "$CLOUD_HISTFILE" <"$LOCAL_HISTFILE"
 
 histutils --format fish "$CLOUD_HISTFILES"/*.{zsh-history,fish-history} >"$tmpfile"
-# TMP_COUNT=$(wc -l <"$tmpfile")
-# DIFF_COUNT=$((TMP_COUNT - LOCAL_COUNT))
+TMP_COUNT=$(wc -l <"$tmpfile")
+DIFF_COUNT=$((TMP_COUNT - LOCAL_COUNT))
 
-# if [ "$DIFF_COUNT" -eq 0 ]; then
-#   echo "Nothing new to sync" >&2
-#   exit 0
-# fi
+if [ "$DIFF_COUNT" -eq 0 ]; then
+  echo "Nothing new to sync" >&2
+  exit 0
+fi
 
-# echo "Adding $DIFF_COUNT new lines" >&2
-# sponge "$CLOUD_HISTFILE" <"$tmpfile"
-# sponge "$LOCAL_HISTFILE" <"$tmpfile"
+echo "Adding $DIFF_COUNT new lines" >&2
+sponge "$CLOUD_HISTFILE" <"$tmpfile"
+sponge "$LOCAL_HISTFILE" <"$tmpfile"
