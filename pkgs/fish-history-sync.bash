2b17a03ed4eba1b40f2a0222240f4a362ce59e7a
Joshua Peek <josh@users.noreply.github.com>
2025-08-17 17:23:24 -0700

    Sync without tempfiles

--- pkgs/fish-history-sync.bash
+++ pkgs/fish-history-sync.bash
@@ -1,27 +1,7 @@
-tmpfile="$(mktemp /tmp/fish_history.XXX)"
-cleanup() {
-  rm -f "$tmpfile"
-}
-trap cleanup EXIT
-trap cleanup SIGINT
-
 CLOUD_HISTFILES="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Terminal/history"
 LOCAL_HISTFILE="$HOME/.local/share/fish/fish_history"
 HOSTNAME=$(hostname -s | tr '[:upper:]' '[:lower:]')
 CLOUD_HISTFILE="$CLOUD_HISTFILES/$HOSTNAME.fish-history"
-LOCAL_COUNT=$(wc -l <"$LOCAL_HISTFILE")
 
-sponge "$CLOUD_HISTFILE" <"$LOCAL_HISTFILE"
-
-histutils --format fish "$CLOUD_HISTFILES"/*.{zsh-history,fish-history} >"$tmpfile"
-TMP_COUNT=$(wc -l <"$tmpfile")
-DIFF_COUNT=$((TMP_COUNT - LOCAL_COUNT))
-
-if [ "$DIFF_COUNT" -eq 0 ]; then
-  echo "Nothing new to sync" >&2
-  exit 0
-fi
-
-echo "Adding $DIFF_COUNT new lines" >&2
-sponge "$CLOUD_HISTFILE" <"$tmpfile"
-sponge "$LOCAL_HISTFILE" <"$tmpfile"
+histutils --format fish "$LOCAL_HISTFILE" "$CLOUD_HISTFILES"/*.{zsh-history,fish-history} | sponge "$LOCAL_HISTFILE"
+cp "$LOCAL_HISTFILE" "$CLOUD_HISTFILE"
--- pkgs/zsh-history-sync.bash
+++ pkgs/zsh-history-sync.bash
@@ -1,27 +1,7 @@
-tmpfile="$(mktemp /tmp/zsh_history.XXX)"
-cleanup() {
-  rm -f "$tmpfile"
-}
-trap cleanup EXIT
-trap cleanup SIGINT
-
 CLOUD_HISTFILES="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Terminal/history"
 LOCAL_HISTFILE="$HOME/.zsh_history"
 HOSTNAME=$(hostname -s | tr '[:upper:]' '[:lower:]')
 CLOUD_HISTFILE="$CLOUD_HISTFILES/$HOSTNAME.zsh-history"
-LOCAL_COUNT=$(wc -l <"$LOCAL_HISTFILE")
 
-sponge "$CLOUD_HISTFILE" <"$LOCAL_HISTFILE"
-
-histutils --format zsh "$CLOUD_HISTFILES"/*.{zsh-history,fish-history} >"$tmpfile"
-TMP_COUNT=$(wc -l <"$tmpfile")
-DIFF_COUNT=$((TMP_COUNT - LOCAL_COUNT))
-
-if [ "$DIFF_COUNT" -eq 0 ]; then
-  echo "Nothing new to sync" >&2
-  exit 0
-fi
-
-echo "Adding $DIFF_COUNT new lines" >&2
-sponge "$CLOUD_HISTFILE" <"$tmpfile"
-sponge "$LOCAL_HISTFILE" <"$tmpfile"
+histutils --format zsh "$LOCAL_HISTFILE" "$CLOUD_HISTFILES"/*.{zsh-history,fish-history} | sponge "$LOCAL_HISTFILE"
+cp "$LOCAL_HISTFILE" "$CLOUD_HISTFILE"
