bcdf06a9dae3bf9966334db45a3217ce38291db1
Joshua Peek <josh@users.noreply.github.com>
2025-08-27 15:10:53 -0700

    New flake lockfile compare script
    Closes #303

---
+++ pkgs/gh-actions-compare-pr-flake-lockfile.bash
@@ -0,0 +1,48 @@
+[ -n "$GITHUB_OUTPUT" ]
+[ -n "$GH_TOKEN" ]
+[ -n "$PR_URL" ]
+[ -n "$BASE_FLAKE" ]
+[ -n "$HEAD_FLAKE" ]
+
+a_out=$(mktemp)
+b_out=$(mktemp)
+
+trap 'rm -f "$a_out" "$b_out"' EXIT
+
+nix eval --json "${BASE_FLAKE}#packages" >"$a_out" &
+pid_a=$!
+
+nix eval --json "${HEAD_FLAKE}#packages" >"$b_out" &
+pid_b=$!
+
+wait $pid_a
+status_a=$?
+wait $pid_b
+status_b=$?
+
+if [ $status_a -ne 0 ]; then
+  echo "::error::failed to evaluate ${BASE_FLAKE}#packages" >&2
+  echo "status=error" >>"$GITHUB_OUTPUT"
+  exit $status_a
+fi
+
+if [ $status_b -ne 0 ]; then
+  echo "::error::failed to evaluate ${HEAD_FLAKE}#packages" >&2
+  echo "status=error" >>"$GITHUB_OUTPUT"
+  exit $status_b
+fi
+
+if jd -color "$a_out" "$b_out"; then
+  echo "status=no-change" >>"$GITHUB_OUTPUT"
+  if ! gh pr edit "$PR_URL" --add-label "noop"; then
+    echo "::warning::Error adding label" >&2
+  fi
+  echo "::notice::No changes detected" >&2
+  exit 1
+else
+  echo "status=change" >>"$GITHUB_OUTPUT"
+  if ! gh pr edit "$PR_URL" --remove-label "noop"; then
+    echo "::warning::Error removing label" >&2
+  fi
+  exit 0
+fi
