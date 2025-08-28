598e0cb9c8de45eea226a68eca38297f676b614b
Joshua Peek <josh@users.noreply.github.com>
2025-08-28 10:11:41 -0700

    Drop xtrace

--- pkgs/zsh-session-prune.bash
+++ pkgs/zsh-session-prune.bash
@@ -3,8 +3,6 @@ shopt -s nullglob
 sessions=("$HOME/.zsh_sessions"/*.history)
 [ "${#sessions[@]}" -eq 0 ] && exit 0
 
-set -o xtrace
-
 histutils \
   --output-format zsh \
   --output "$HOME/.zsh_history" \
