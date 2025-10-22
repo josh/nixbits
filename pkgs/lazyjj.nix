70c63a7764f7b2196d156ef46b1e8cafc24205c4
Joshua Peek <josh@users.noreply.github.com>
2025-10-22 16:10:48 -0700

    Work around lazyjj bug

--- pkgs/lazyjj.nix
+++ pkgs/lazyjj.nix
@@ -47,11 +47,22 @@ stdenvNoCC.mkDerivation {
     lndir
   ];
 
+  # <https://github.com/Cretezy/lazyjj/commit/523c422e6bc4c86fffcdd68eb3900085dcf23e19>
+  disableCSI =
+    assert lib.asserts.assertMsg (
+      (builtins.compareVersions "0.6.1" lazyjj.version) == 0
+    ) "lazyjj CSI patch no longer needed";
+    ''
+      printf '\033[<u'
+    '';
+
   buildCommand = ''
     mkdir $out
     lndir -silent ${lazyjj'} $out
 
     rm $out/bin/lazyjj
     makeWrapper ${lazyjj'}/bin/lazyjj $out/bin/lazyjj "''${makeWrapperArgs[@]}"
+    sed -i 's/^exec //' $out/bin/lazyjj
+    echo "$disableCSI" >>$out/bin/lazyjj
   '';
 }
