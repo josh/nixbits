e247b95b8a47eccc293f0c6c2ea21085fdd223b9
Joshua Peek <josh@users.noreply.github.com>
2026-01-10 21:30:27 -0800

    Continue on rebuild failures

--- pkgs/nix-flake-rebuild.bash
+++ pkgs/nix-flake-rebuild.bash
@@ -4,6 +4,6 @@ nix flake show --quiet --json 2>/dev/null |
   jq --raw-output --arg system "$system" '.packages[$system] | keys | .[]' |
   while read -r drv; do
     echo "+ nix build --rebuild .#$drv"
-    nix build --no-link ".#$drv"
-    nix build --rebuild --no-link ".#$drv"
+    nix build --no-link ".#$drv" || true
+    nix build --rebuild --no-link ".#$drv" || true
   done
