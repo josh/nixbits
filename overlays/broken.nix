7bce0c8d5f14219e4ad12761a15bc33e07e0c372
Joshua Peek <josh@users.noreply.github.com>
2026-03-06 17:35:56 -0800

    Fallback if pkg set is missing

--- overlays/broken.nix
+++ overlays/broken.nix
@@ -1,43 +1,39 @@
 _final: prev:
-let
-  inherit (prev) lib;
-  inherit (prev.stdenv.hostPlatform) system;
-
-  isRelease2511 = lib.trivial.release == "25.11";
-  isUnstable = lib.trivial.release == "26.05";
-
-  # markBroken =
-  #   drv:
-  #   drv.overrideAttrs (
-  #     {
-  #       meta ? { },
-  #       ...
-  #     }:
-  #     {
-  #       meta = meta // {
-  #         broken = true;
-  #       };
-  #     }
-  #   );
-in
-if isRelease2511 && system == "x86_64-linux" then
-  {
-  }
-else if isRelease2511 && system == "aarch64-linux" then
-  {
-  }
-else if isRelease2511 && system == "aarch64-darwin" then
-  {
-  }
-else if isUnstable && system == "x86_64-linux" then
-  {
-  }
-else if isUnstable && system == "aarch64-linux" then
-  {
-  }
-else if isUnstable && system == "aarch64-darwin" then
-  {
-  }
+if !(prev ? lib && prev ? stdenv) then
+  { }
 else
-  {
-  }
+  let
+    inherit (prev) lib;
+    inherit (prev.stdenv.hostPlatform) system;
+
+    isRelease2511 = lib.trivial.release == "25.11";
+    isUnstable = lib.trivial.release == "26.05";
+
+    # markBroken =
+    #   drv:
+    #   drv.overrideAttrs (
+    #     {
+    #       meta ? { },
+    #       ...
+    #     }:
+    #     {
+    #       meta = meta // {
+    #         broken = true;
+    #       };
+    #     }
+    #   );
+  in
+  if isRelease2511 && system == "x86_64-linux" then
+    { }
+  else if isRelease2511 && system == "aarch64-linux" then
+    { }
+  else if isRelease2511 && system == "aarch64-darwin" then
+    { }
+  else if isUnstable && system == "x86_64-linux" then
+    { }
+  else if isUnstable && system == "aarch64-linux" then
+    { }
+  else if isUnstable && system == "aarch64-darwin" then
+    { }
+  else
+    { }
