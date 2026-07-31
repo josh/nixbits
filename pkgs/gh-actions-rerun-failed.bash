ad8ba851b8d782abdcba5b607f3907a0ca390628
Joshua Peek <josh@users.noreply.github.com>
2026-07-31 08:00:18 -0700

    pkgs/gh-scripts: fail when GitHub API calls fail

--- pkgs/gh-actions-rerun-failed.bash
+++ pkgs/gh-actions-rerun-failed.bash
@@ -1,10 +1,16 @@
 failed=0
-mapfile -t repos < <(gh repo list josh --json 'nameWithOwner' --no-archived --limit 1000 --jq '.[] | .nameWithOwner')
+repos_output=$(gh repo list josh --json 'nameWithOwner' --no-archived --limit 1000 --jq '.[] | .nameWithOwner')
+repos=()
+[ -z "$repos_output" ] || mapfile -t repos <<<"$repos_output"
 for repo in "${repos[@]}"; do
   echo "Processing ${repo}..." >&2
-  mapfile -t workflows < <(gh workflow list --repo "$repo" --json 'id' --jq '.[] | .id')
+  workflows_output=$(gh workflow list --repo "$repo" --json 'id' --jq '.[] | .id')
+  workflows=()
+  [ -z "$workflows_output" ] || mapfile -t workflows <<<"$workflows_output"
   for workflow in "${workflows[@]}"; do
-    mapfile -t runs < <(gh run list --repo "$repo" --workflow "$workflow" --branch main --limit 1 --json 'databaseId,conclusion' --jq 'map(select(.conclusion == "failure"))[] | .databaseId')
+    runs_output=$(gh run list --repo "$repo" --workflow "$workflow" --branch main --limit 1 --json 'databaseId,conclusion' --jq 'map(select(.conclusion == "failure"))[] | .databaseId')
+    runs=()
+    [ -z "$runs_output" ] || mapfile -t runs <<<"$runs_output"
     for run in "${runs[@]}"; do
       echo "+ gh run rerun --repo $repo --failed $run" >&2
       if ! gh run rerun --repo "$repo" --failed "$run"; then
