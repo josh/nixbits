eddb48cd4a1691943d4b8d5d25daa1a893eff993
Joshua Peek <josh@users.noreply.github.com>
2026-07-31 08:17:46 -0700

    pkgs/install-mac-app: use two-argument overrideAttrs for tests

--- pkgs/install-mac-app.nix
+++ pkgs/install-mac-app.nix
@@ -23,42 +23,48 @@ let
     };
   };
 in
-script.overrideAttrs (_finalAttrs: {
-  passthru.tests = {
-    install-dry-run = runCommand "test-install-dry-run" { nativeBuildInputs = [ script ]; } ''
-      mkdir -p "$TMPDIR/Applications"
-      install-mac-app --dry-run --appdir "$TMPDIR/Applications" ${neovide}
-      [ ! -d "$TMPDIR/Applications/Neovide.app" ]
-      touch $out
-    '';
+script.overrideAttrs (
+  finalAttrs: _previousAttrs: {
+    passthru.tests = {
+      install-dry-run =
+        runCommand "test-install-dry-run" { nativeBuildInputs = [ finalAttrs.finalPackage ]; }
+          ''
+            mkdir -p "$TMPDIR/Applications"
+            install-mac-app --dry-run --appdir "$TMPDIR/Applications" ${neovide}
+            [ ! -d "$TMPDIR/Applications/Neovide.app" ]
+            touch $out
+          '';
 
-    install-drv = runCommand "test-install-drv" { nativeBuildInputs = [ script ]; } ''
-      mkdir -p "$TMPDIR/Applications"
-      install-mac-app --appdir "$TMPDIR/Applications" ${neovide}
-      [ -d "$TMPDIR/Applications/Neovide.app" ]
-      [ ! -d "$TMPDIR/Applications/Neovide.app/Neovide.app" ]
-      touch $out
-    '';
+      install-drv = runCommand "test-install-drv" { nativeBuildInputs = [ finalAttrs.finalPackage ]; } ''
+        mkdir -p "$TMPDIR/Applications"
+        install-mac-app --appdir "$TMPDIR/Applications" ${neovide}
+        [ -d "$TMPDIR/Applications/Neovide.app" ]
+        [ ! -d "$TMPDIR/Applications/Neovide.app/Neovide.app" ]
+        touch $out
+      '';
 
-    install-app = runCommand "test-install-app" { nativeBuildInputs = [ script ]; } ''
-      mkdir -p "$TMPDIR/Applications"
-      install-mac-app --appdir "$TMPDIR/Applications" ${neovide}/Applications/Neovide.app
-      [ -d "$TMPDIR/Applications/Neovide.app" ]
-      [ ! -d "$TMPDIR/Applications/Neovide.app/Neovide.app" ]
-      touch $out
-    '';
+      install-app = runCommand "test-install-app" { nativeBuildInputs = [ finalAttrs.finalPackage ]; } ''
+        mkdir -p "$TMPDIR/Applications"
+        install-mac-app --appdir "$TMPDIR/Applications" ${neovide}/Applications/Neovide.app
+        [ -d "$TMPDIR/Applications/Neovide.app" ]
+        [ ! -d "$TMPDIR/Applications/Neovide.app/Neovide.app" ]
+        touch $out
+      '';
 
-    install-logging = runCommand "test-install-logging" { nativeBuildInputs = [ script ]; } ''
-      mkdir -p "$TMPDIR/Applications"
-      if ! install-mac-app --appdir "$TMPDIR/Applications" ${neovide} 2>&1 | grep '+ rsync'; then
-        echo "error: rsync command not logged"
-        exit 1
-      fi
-      if install-mac-app --appdir "$TMPDIR/Applications" ${neovide} 2>&1 | grep '+ rsync'; then
-        echo "error: rsync command logged"
-        exit 1
-      fi
-      touch $out
-    '';
-  };
-})
+      install-logging =
+        runCommand "test-install-logging" { nativeBuildInputs = [ finalAttrs.finalPackage ]; }
+          ''
+            mkdir -p "$TMPDIR/Applications"
+            if ! install-mac-app --appdir "$TMPDIR/Applications" ${neovide} 2>&1 | grep '+ rsync'; then
+              echo "error: rsync command not logged"
+              exit 1
+            fi
+            if install-mac-app --appdir "$TMPDIR/Applications" ${neovide} 2>&1 | grep '+ rsync'; then
+              echo "error: rsync command logged"
+              exit 1
+            fi
+            touch $out
+          '';
+    };
+  }
+)
