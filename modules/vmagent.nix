bf5a1bc2f35f3586ced90a6d227211aa2a326729
Joshua Peek <josh@users.noreply.github.com>
2026-07-31 08:10:50 -0700

    modules/vmagent: only apply configuration when enabled

--- modules/vmagent.nix
+++ modules/vmagent.nix
@@ -4,38 +4,42 @@
   ...
 }:
 {
-  config = {
-    services.vmagent.enable = lib.modules.mkDefault (config.services.vmagent.remoteWrite.url != null);
-    services.vmagent.extraArgs = [ "-httpListenAddr=127.0.0.1:8429" ];
+  config = lib.modules.mkMerge [
+    {
+      services.vmagent.enable = lib.modules.mkDefault (config.services.vmagent.remoteWrite.url != null);
+    }
+    (lib.modules.mkIf config.services.vmagent.enable {
+      services.vmagent.extraArgs = [ "-httpListenAddr=127.0.0.1:8429" ];
 
-    services.vmagent.prometheusConfig.scrape_configs =
-      # https://docs.victoriametrics.com/victoriametrics/vmagent/#monitoring
-      (lib.lists.optional config.services.vmagent.enable {
-        job_name = "vmagent";
-        scrape_interval = "30s";
-        static_configs = [
-          { targets = [ "127.0.0.1:8429" ]; }
-        ];
-        # BUG: NixOS builds vmagent without program in version prefix
-        # https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1785
-        metric_relabel_configs = [
-          {
-            source_labels = [
-              "__name__"
-              "version"
-            ];
-            regex = "vm_app_version;([0-9].*)";
-            target_label = "version";
-            replacement = "vmagent-$1";
-          }
-        ];
-        relabel_configs = [
-          {
-            target_label = "instance";
-            replacement = config.networking.hostName;
-          }
-        ];
-      })
+      services.vmagent.prometheusConfig.scrape_configs = [
+        # https://docs.victoriametrics.com/victoriametrics/vmagent/#monitoring
+        {
+          job_name = "vmagent";
+          scrape_interval = "30s";
+          static_configs = [
+            { targets = [ "127.0.0.1:8429" ]; }
+          ];
+          # BUG: NixOS builds vmagent without program in version prefix
+          # https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1785
+          metric_relabel_configs = [
+            {
+              source_labels = [
+                "__name__"
+                "version"
+              ];
+              regex = "vm_app_version;([0-9].*)";
+              target_label = "version";
+              replacement = "vmagent-$1";
+            }
+          ];
+          relabel_configs = [
+            {
+              target_label = "instance";
+              replacement = config.networking.hostName;
+            }
+          ];
+        }
+      ]
       # https://docs.victoriametrics.com/victorialogs/vlagent/#monitoring
       ++ (lib.lists.optional config.services.vlagent.enable {
         job_name = "vlagent";
@@ -91,12 +95,13 @@
         ];
       });
 
-    services.prometheus.exporters.node = {
-      enable = lib.modules.mkDefault config.services.vmagent.enable;
-      enabledCollectors = [
-        "processes"
-        "systemd"
-      ];
-    };
-  };
+      services.prometheus.exporters.node = {
+        enable = lib.modules.mkDefault true;
+        enabledCollectors = [
+          "processes"
+          "systemd"
+        ];
+      };
+    })
+  ];
 }
