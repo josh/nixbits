1ac0fdb5bafb780b083ebe97f1f542523f19ccce
Joshua Peek <josh@users.noreply.github.com>
2026-07-31 18:03:34 -0700

    pkgs/jujutsu-bookmark-clean: fail when bookmark listing fails

--- pkgs/jujutsu-bookmark-clean.bash
+++ pkgs/jujutsu-bookmark-clean.bash
@@ -1,7 +1,9 @@
 # shellcheck source=/dev/null
 source "$XTRACE_PATH/share/bash/xtrace.bash"
 
-readarray -t bookmarks < <(jj bookmark list --revisions '::trunk()' --template 'name ++ "\n"')
+bookmarks_output=$(jj bookmark list --revisions '::trunk()' --template 'name ++ "\n"')
+bookmarks=()
+[ -z "$bookmarks_output" ] || readarray -t bookmarks <<<"$bookmarks_output"
 
 for bookmark in "${bookmarks[@]}"; do
   if [[ $bookmark == "main" || $bookmark == "master" ]]; then
