a4cbc4e1de68b010178c415c2bd426f5fdd5d346
Joshua Peek <josh@users.noreply.github.com>
2026-09-08 12:58:50 -0700

    Wrap wget with nix-wrapper-modules

--- pkgs/wget.nix
+++ pkgs/wget.nix
@@ -1,25 +1,4 @@
 {
-  lib,
-  symlinkJoin,
-  makeWrapper,
-  wget,
+  wlibEvalPackage,
 }:
-symlinkJoin {
-  inherit (wget) name;
-  paths = [ wget ];
-  nativeBuildInputs = [ makeWrapper ];
-  postBuild = ''
-    rm $out/bin/wget
-    makeWrapper ${lib.getExe wget} $out/bin/wget \
-      --add-flags '--hsts-file=''${XDG_DATA_HOME:-$HOME/.local/share}/wget-hsts'
-  '';
-  meta = {
-    inherit (wget.meta)
-      description
-      homepage
-      license
-      mainProgram
-      platforms
-      ;
-  };
-}
+wlibEvalPackage ../modules/wget-wrapper.nix
